52be098c7d280621d05fcbdc44708a93644e7f5e
kenshay <kennyshay123@gmail.com>
2019-04-10 16:07:12 -0400

    asd

---
+++ EXE/Reset_Image_Scripter_2.py
@@ -0,0 +1,14 @@
+import os
+print("Starting Reset")
+gitProject = r"C:\ImageScripter_2"
+git = "C:\Elan_Tools\Sync\cmd\git.exe"
+os.chdir(gitProject)
+os.system(git + ' reset --hard origin/master')
+os.system(git + ' fetch --all')
+os.system(git + ' reset --hard origin/master')
+os.system(git + ' status')
+input("Finished Reset")
+
+
+
+
