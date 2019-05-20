6c1c8b7494d5f4fe2c2a4683e9fa289a520044c4
kenshay <kennyshay123@gmail.com>
2019-05-20 11:19:28 -0400

    asd

--- EXE/Reset_Image_Scripter_2.py
+++ EXE/Reset_Image_Scripter_2.py
@@ -1,4 +1,18 @@
 import os
+import shutil, errno
+
+elan_folder = r"C:\ImageScripter_2\Lib\site-packages\elan"
+old_elan = r"C:\Settings\elan"
+
+
+def copyanything(src, dst):
+    try:
+        shutil.copytree(src, dst)
+    except OSError as exc: # python >2.5
+        if exc.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        else: raise
+
 print("Starting Reset")
 gitProject = r"C:\ImageScripter_2"
 git = "C:\Elan_Tools\Sync\cmd\git.exe"
@@ -8,5 +22,10 @@ os.system(git + ' fetch --all')
 os.system(git + ' reset --hard origin/master')
 os.system(git + ' clean -f')
 os.system(git + ' status')
+try:
+    os.rmdir(old_elan)
+except Exception as e:
+    print(e)
+copyanything(elan_folder, old_elan)
 input("Finished Reset")
 
