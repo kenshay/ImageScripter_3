f7439fa7cb161b81f4b42f69f00da632c3502513
Kenny <kennyshay123@gmail.com>
2019-08-07 17:16:56 -0400

    asd

--- EXE/Script_Writer.py
+++ EXE/Script_Writer.py
@@ -1,5 +1,14 @@
-import os
-from paths import *
-path = Python_Exe_Location + ' ' + ImageScripter_Location + "Lib\site-packages\Script_Writer\script_writer.py"
-##os.system(r"C:\ImageScripter_2\python.exe C:\ImageScripter_2\Lib\site-packages\Script_Writer\script_writer.py")
-os.system(path)
+
+try:
+    import os
+    from paths import *
+    path = Python_Exe_Location + ' ' + ImageScripter_Location + "Lib\site-packages\Script_Writer\script_writer.py"
+    ##os.system(r"C:\ImageScripter_2\python.exe C:\ImageScripter_2\Lib\site-packages\Script_Writer\script_writer.py")
+    os.system(path)
+
+except Exception as e:
+    print(e)
+    input()
+
+
+#C:\ImageScripter_3\EXE\Script_Writer.exeLib\site-packages\Script_Writer\script_writer.py
