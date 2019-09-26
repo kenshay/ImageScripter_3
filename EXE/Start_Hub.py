c0682aa75cb6d767bc4d7fa6be9931ff9dabbb4d
Kenny <kennyshay123@gmail.com>
2019-09-26 15:29:45 -0400

    'asd'

---
+++ EXE/Start_Hub.py
@@ -0,0 +1,14 @@
+
+try:
+    import os
+    from paths import *
+    path = Python_Exe_Location + ' ' + ImageScripter_Location + "Lib\site-packages\Hub\hub.py"
+    ##os.system(r"C:\ImageScripter_2\python.exe C:\ImageScripter_2\Lib\site-packages\Script_Writer\script_writer.py")
+    os.system(path)
+
+except Exception as e:
+    print(e)
+    input()
+
+
+#C:\ImageScripter_3\EXE\Script_Writer.exeLib\site-packages\Script_Writer\script_writer.py
