91479f2f8fedf998e67fcf1510b88fe12cb627e3
Ken Shay <Ken.Shay@nortekcontrol.com>
2019-12-12 16:12:07 -0500

    'asd'

--- EXE/Script_Runner_GUI_On_Startup.py
+++ EXE/Script_Runner_GUI_On_Startup.py
@@ -1,7 +1,7 @@
 import os
 from paths import *
 ##os.system(r'C:\ImageScripter_2\python.exe C:\ImageScripter_2\Lib\site-packages\Script_Writer\script_writer.py C:\ImageScripter_2\Lib\site-packages\imagescripter\core\script_runner_class.py run')
-os.system(Python_Exe_Location + ' ' + 'C:\Eluminate\System\ImageScripter\Lib\site-packages\Script_Runner\script_runner_gui.py' + ' True')
+os.system(Python_Exe_Location + ' ' + Script_Runner_Gui_Location + ' True')
 
 
 
