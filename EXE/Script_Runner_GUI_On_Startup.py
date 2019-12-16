d59dd073eb63192aef7e58fb8a28d6cd58750362
Ken Shay <Ken.Shay@nortekcontrol.com>
2019-12-16 13:11:54 -0500

    'asd'

--- EXE/Script_Runner_GUI_On_Startup.py
+++ EXE/Script_Runner_GUI_On_Startup.py
@@ -1,7 +1,30 @@
 import os
 from paths import *
-##os.system(r'C:\ImageScripter_2\python.exe C:\ImageScripter_2\Lib\site-packages\Script_Writer\script_writer.py C:\ImageScripter_2\Lib\site-packages\imagescripter\core\script_runner_class.py run')
-os.system(Python_Exe_Location + ' ' + Script_Runner_Gui_Location + ' True')
 
+
+
+try:
+    import socket
+    import pickle
+    HOST = '127.0.0.1'
+    PORT = 65432
+    isRunning = False
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        #s.sendall(b'close()')
+        s.sendall(b'isRunning()')
+        data = s.recv(1024)
+        #msg = data.decode()
+        msg = data
+        isRunning = pickle.loads(msg)
+except Exception as e:
+    print(e)
+    isRunning = False
+
+if isRunning == False:
+    print('Script Runner is not running, starting,')
+    os.system(Python_Exe_Location + ' ' + Script_Runner_Gui_Location + ' True')
+else:
+    print('Script Runner is already running, ignoring.')
 
 
