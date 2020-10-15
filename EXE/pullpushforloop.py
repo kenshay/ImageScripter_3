46cc4d39240edd7f15eb89f9d5439db85f6f53ed
Ken shay <kennyshay123@gmail.com>
2020-10-15 11:00:45 -0400

    auto update

--- EXE/pullpushforloop.py
+++ EXE/pullpushforloop.py
@@ -2,22 +2,12 @@ from paths import *
 import os
 import shutil, errno
 from dirsync import sync
-
-
 print('Git Repo -> ',gitProject)
 os.chdir(gitProject)
-
-
-
-
 os.system(git + ' pull')
 os.system(git + ' add --all')
 os.system(git + ' commit -a -m "auto update"')
 os.system(git + ' push')
 os.system(git + ' status')
-#os.system(git + ' fetch --all')
-#os.system(git + ' reset --hard origin/master')
-#os.system(git + ' clean -f')
-#os.system(git + ' status')
-#print('Please do not close this window.')
+
 
